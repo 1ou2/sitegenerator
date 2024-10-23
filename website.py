436b4b67e111ebd16b18087b008adfec8ec7d2f9
gabriel pastor <github@1ou2.com>
2024-10-23 15:21:53 +0200

    conf

--- website.py
+++ website.py
@@ -3,17 +3,33 @@ import os,shutil
 from dotenv import load_dotenv
 from articles import Article
 
+class Configuration:
+    def __init__(self):
+        load_dotenv()
+        self.md_dir = os.getenv("MARKDOWN_DIR")
+        self.html_dir = os.getenv("HTML_DIR")
+        self.top_tags = int(os.getenv("TOP_TAGS", 10))
+        self.nb_articles_per_page = int(os.getenv("NB_ARTICLES_PER_PAGE", 5))
+        self.config = {
+            "md_dir": self.md_dir,
+            "html_dir": self.html_dir,
+            "top_tags": self.top_tags,
+            "nb_articles_per_page": self.nb_articles_per_page
+        }
+
+    def get(self, key):
+        return self.config[key]
+    
 class Website:
-    def __init__(self,md_dir,html_dir):
-        self.md_dir = md_dir
-        self.html_dir = html_dir
+    def __init__(self,conf):
+        self.config = conf
         self.articles = []
         self.articles_by_tag = {}
 
     # get list of all markdown files
     def get_markdown_files(self):
         md_files = []
-        for root, dirs, files in os.walk(self.md_dir):
+        for root, dirs, files in os.walk(self.config.md_dir):
             for file in files:
                 if file.endswith(".md"):
                     md_files.append(os.path.join(root, file))
@@ -22,19 +38,19 @@ class Website:
     # clean html dir
     def clean_html_dir(self):
         # delete html dir
-        shutil.rmtree(self.html_dir,ignore_errors=True)
-        os.makedirs(self.html_dir)
+        shutil.rmtree(self.config.html_dir,ignore_errors=True)
+        os.makedirs(self.config.html_dir)
 
     def init_html(self):
         self.clean_html_dir()
         # copy md_dir to html_dir  # cp -r md_dir/* html_dir
-        shutil.copytree(self.md_dir, self.html_dir, dirs_exist_ok=True)
+        shutil.copytree(self.config.md_dir, self.config.html_dir, dirs_exist_ok=True)
         # copy assets to html_dir/assets
-        assets_dir = os.path.join(self.html_dir, "assets")
+        assets_dir = os.path.join(self.config.html_dir, "assets")
         os.makedirs(assets_dir, exist_ok=True)
         shutil.copytree("assets", assets_dir, dirs_exist_ok=True)
         # copy static files to html_dir
-        static_dir = os.path.join(self.html_dir)
+        static_dir = os.path.join(self.config.html_dir)
         os.makedirs(static_dir, exist_ok=True)
         shutil.copytree("static", static_dir, dirs_exist_ok=True)
 
@@ -66,7 +82,6 @@ class Website:
 
     def get_top_tags(self,top):
         """ Return the top most used tags """
-
         return self.sorted_tags[:top]
 
     def get_template(self,filename):
@@ -75,16 +90,23 @@ class Website:
         with open(filename, 'r') as file:
             return file.read()
     
+    def get_articles_by_tag(self, tag):
+        return self.articles_by_tag.get(tag, [])
+    
+    def get_articles_by_date(self):
+        # sort articles by date
+        return sorted(self.articles, key=lambda x: x.date, reverse=True)
+    
     # create html file, in html_dir, by transforming the markdown into html
     def generate_html_article(self,article):
         # md files were copied in the html directory, we currently have 
         # html_dir/YYYY/MM/DD/article_dir/article.md
         # we will create html_dir/YYYY/MM/DD/article_dir/article.html
-        html_file_path = article.md_file_path.replace(self.md_dir, self.html_dir).replace(".md", ".html")
+        html_file_path = article.md_file_path.replace(self.config.md_dir, self.config.html_dir).replace(".md", ".html")
         # css file path
         # html article is in html_dir/YYYY/MM/DD/article_dir/article.html
         # css file is in html_dir/assets/style.css
-        css_path = os.path.join("html","assets", "multicolor.css")
+        css_path = os.path.join(self.config.html_dir,"assets", "multicolor.css")
         # variables used in the template
         css_rel_path = os.path.relpath(css_path, os.path.dirname(html_file_path))
         title = article.title
@@ -104,14 +126,11 @@ class Website:
     def generate_index(self):
         pass
 
-        
+    
 
 if __name__ == "__main__":
-    load_dotenv()
-    md_dir = os.getenv("MARKDOWN_DIR")
-    html_dir = os.getenv("HTML_DIR")
-
-    www = Website(md_dir, html_dir)
+    conf = Configuration()
+    www = Website(conf)
     www.init_html()
     www.init_articles()
 
